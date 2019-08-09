98dc691161649db0e8094899415000064f5b50ad
Hex25 <natasha.minin@gmail.com>
2019-08-09 10:46:19 +0200

    added logic for toolbox

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
+// toolbox
+
+const buttonChecker = document.getElementById('button-checker');
+const buttonPicker = document.getElementById('button-picker');
+const buttonConverter = document.getElementById('button-converter');
+const sizeChecker = document.getElementById('size-checker');
+const colorPicker = document.getElementById('color-picker');
+const emConverter = document.getElementById('em-converter');
+
+buttonChecker.onclick = function(){
+    buttonPicker.style.backgroundColor = 'lightgrey';
+    buttonChecker.style.backgroundColor = 'rgb(174, 228, 250)';
+    buttonConverter.style.backgroundColor = 'lightgrey';
+    buttonPicker.style.borderBottom = 'solid';
+    buttonChecker.style.borderBottom = 'none';
+    buttonConverter.style.borderBottom = 'solid';
+    sizeChecker.style.display = 'block';
+    colorPicker.style.display = 'none';
+    emConverter.style.display = 'none';
+}
+
+buttonPicker.onclick = function(){
+    buttonPicker.style.backgroundColor = 'rgb(174, 228, 250)';
+    buttonChecker.style.backgroundColor = 'lightgrey';
+    buttonConverter.style.backgroundColor = 'lightgrey';
+    buttonPicker.style.borderBottom = 'none';
+    buttonChecker.style.borderBottom = 'solid';
+    buttonConverter.style.borderBottom = 'solid';
+    sizeChecker.style.display = 'none';
+    colorPicker.style.display = 'block';
+    emConverter.style.display = 'none';
+}
+
+buttonConverter.onclick = function(){
+    buttonPicker.style.backgroundColor = 'lightgrey';
+    buttonChecker.style.backgroundColor = 'lightgrey';
+    buttonConverter.style.backgroundColor = 'rgb(174, 228, 250)';
+    buttonPicker.style.borderBottom = 'solid';
+    buttonChecker.style.borderBottom = 'solid';
+    buttonConverter.style.borderBottom = 'none';
+    sizeChecker.style.display = 'none';
+    colorPicker.style.display = 'none';
+    emConverter.style.display = 'block';
+}
+
+
+// screen size checker
+
 let w = window.innerWidth;
 let h = window.innerHeight;
 
@@ -10,3 +58,8 @@ document.querySelector('body').onresize = function(){
     document.getElementById('size').innerHTML = `${w} X 
 ${h}`;
 };
+
+// color picker
+
+
+// em converter
