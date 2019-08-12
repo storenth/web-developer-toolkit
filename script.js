2454fc33f7da69cead5ab2b677bd1d56ddb81891
Natasha Minin <natasha.minin@gmail.com>
2019-08-12 16:59:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,75 +1,72 @@
-// toolbox
-
-const buttonChecker = document.getElementById('button-checker');
-const buttonPicker = document.getElementById('button-picker');
-const buttonConverter = document.getElementById('button-converter');
-const sizeChecker = document.getElementById('size-checker');
-const colorPicker = document.getElementById('color-picker');
-const emConverter = document.getElementById('em-converter');
-
-buttonChecker.onclick = function(){
-    buttonPicker.style.backgroundColor = 'lightgrey';
-    buttonChecker.style.backgroundColor = 'rgb(174, 228, 250)';
-    buttonConverter.style.backgroundColor = 'lightgrey';
-    buttonPicker.style.borderBottom = 'solid';
-    buttonChecker.style.borderBottom = 'none';
-    buttonConverter.style.borderBottom = 'solid';
-    sizeChecker.style.display = 'block';
-    colorPicker.style.display = 'none';
-    emConverter.style.display = 'none';
+/* reset default styles */ 
+* {
+    box-sizing: border-box;
+  }
+*{
+    margin: 0;
+    padding: 0;
 }
 
-buttonPicker.onclick = function(){
-    buttonPicker.style.backgroundColor = 'rgb(174, 228, 250)';
-    buttonChecker.style.backgroundColor = 'lightgrey';
-    buttonConverter.style.backgroundColor = 'lightgrey';
-    buttonPicker.style.borderBottom = 'none';
-    buttonChecker.style.borderBottom = 'solid';
-    buttonConverter.style.borderBottom = 'solid';
-    sizeChecker.style.display = 'none';
-    colorPicker.style.display = 'block';
-    emConverter.style.display = 'none';
+body {
+   background-color: rgb(174, 228, 250);
+   text-align: center;
 }
 
-buttonConverter.onclick = function(){
-    buttonPicker.style.backgroundColor = 'lightgrey';
-    buttonChecker.style.backgroundColor = 'lightgrey';
-    buttonConverter.style.backgroundColor = 'rgb(174, 228, 250)';
-    buttonPicker.style.borderBottom = 'solid';
-    buttonChecker.style.borderBottom = 'solid';
-    buttonConverter.style.borderBottom = 'none';
-    sizeChecker.style.display = 'none';
-    colorPicker.style.display = 'none';
-    emConverter.style.display = 'block';
+#main {
+   font-family: 'Blinker', sans-serif;
 }
 
+/* the toolbox */
+#toolbox {
+    margin-top: 30vh;
+}
+#size-checker, #color-picker, #em-converter {
+    font-size: 3rem;
+    border: 2px solid black;
+    border-top: none;
+    border-radius: 0 0 10px 10px;
+    padding: 20px;
+    margin: 0 auto;
+    width: 600px;
+    min-height: 300px;
+}
+#color-picker, #em-converter {
+    display: none;
+}
+#size {
+    font-size: 4rem;
+}
 
-// screen size checker
+/* buttons */
 
-let w = window.innerWidth;
-let h = window.innerHeight;
-
-document.getElementById('size').innerHTML = `${w} X 
-${h}`;
-
-document.querySelector('body').onresize = function(){
-    w = window.innerWidth;
-    h = window.innerHeight;
-    document.getElementById('size').innerHTML = `${w} X 
-${h}`;
-};
-
-// color picker
-document.querySelector('#color').addEventListener('click', e => {
-    const rColor = Math.floor(Math.random() * 256);
-    const gColor = Math.floor(Math.random() * 256);
-    const bColor = Math.floor(Math.random() * 256);
-    const opacityColor = parseFloat(Math.random().toFixed(1));
-
-    const bodyBackgroundColor = "rgba(" + rColor + "," + gColor + "," + bColor + ", " + opacityColor + ")";
-
-    document.body.style.background = bodyBackgroundColor;
-    document.querySelector('#color-picker').innerHTML = `Your background Color is: ${bodyBackgroundColor}`;
-});
-
-// em converter
+#button-checker, #button-picker, #button-converter {
+    display: inline-block;
+    border: 2px solid black;
+    border-bottom: 2px dashed black;
+    border-radius: 5px 5px 0 0;
+    width: 198px;
+    font-size: 1.3em;
+    padding: 5px 0;
+}
+#button-checker {
+    padding-top: 15px;
+}
+#button-picker {
+background-color: lightgrey;
+}
+#button-converter {
+background-color: lightgrey;
+}
+#color-button {
+    background-color: aquamarine;
+    border: 2px solid rosybrown;
+    color: rosybrown;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    border-radius: 10px;
+    font-size: 25px;
+    width: 360px;
+    margin: 20px auto 0 auto;
+    cursor: pointer;
+}
